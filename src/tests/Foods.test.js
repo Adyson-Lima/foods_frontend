cd3a20188ff752f6656ef8b8826e7ee44da90f61
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-05 15:26:23 -0300

    10° commit - tdd tela Foods: Existe botão excluir em Foods? RED

--- src/tests/Foods.test.js
+++ src/tests/Foods.test.js
@@ -28,4 +28,8 @@ describe('Testes da tela Foods', () => {
     expect(screen.getByTestId('mybtn1')).toBeInTheDocument();
   });
 
+  it('Existe botão excluir em Foods?', () => {
+    expect(screen.getByTestId('mybtn2')).toBeInTheDocument();
+  });
+
 });
