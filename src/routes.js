43b8e7f8f7b1e2e05b42f76c958d673e0b88c193
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-05 16:21:41 -0300

    12° commit - tdd tela NewUpdate: Existe card em NewUpdate? RED

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Foods from './pages/Foods';
-// import NewUpdate from './pages/NewUpdate';
+import NewUpdate from './pages/NewUpdate';
 
 export default function FoodsRouter(){
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Foods/>} />
+        <Route path="/newupdate/:food_id" element={<NewUpdate/>} />
       </Routes>
     </BrowserRouter>
   );
